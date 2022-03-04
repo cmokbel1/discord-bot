2207d776dc1fe2d0d3a0b9ed9b54a611545b43ca
Buff4eyes <claudemokbel@gmail.com>
2022-03-04 12:41:59 -0800

    fixed a typo

--- commands/affirm.js
+++ commands/affirm.js
@@ -7,7 +7,7 @@ const quoteables = require('../quotes')
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('affirm')
-    .setDescription('Users can pull receive a random quote of affirmation'),
+    .setDescription('Users can receive a random quote of affirmation'),
   async execute(interaction) {
     let randomQuoteIndex = Math.floor(Math.random() * quoteables.length)
     await interaction.reply(`${interaction.user.username} - Your affirmation is, "${quoteables[randomQuoteIndex].quote}" - ${quoteables[randomQuoteIndex].name}`)
