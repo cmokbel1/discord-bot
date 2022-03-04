e0a9f3dc14e16d46e1ce13236a393a838cb98cae
Buff4eyes <claudemokbel@gmail.com>
2022-03-04 12:38:57 -0800

    help command fix

--- commands/help.js
+++ commands/help.js
@@ -10,21 +10,20 @@ const commands = [
     description: "See a list of commands"
   }]
 
-let commandList = [];
 
-  commands.forEach(command => {
-    command.toString(command.name, command.description)
-    commandList.push(command)
-  })
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('help')
     .setDescription('See a list of commands'),
   async execute(interaction) {
-    commandList.toString()
+    let commandText = ''
+    commands.forEach(command => {
+      commandText += `${command.name} - ${command.description}\n`
+
+    })
     await interaction.reply(
-      `To submit quotes you would like to see added, please visit "https://github.com/cmokbel1/discord-bot" and submit an issues request\n Commands:\n${commandList.command}\n
+      `To submit quotes you would like to see added, please visit "https://github.com/cmokbel1/discord-bot" and submit an issues request\n Commands:\n ${commandText}
       `
     )
   }
