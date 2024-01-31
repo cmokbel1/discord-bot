dcf229d361137c63251fc1cbd84fc42a270f119a
Claude Mokbel <claudemokbel@gmail.com>
2024-01-30 18:41:04 -0800

    remove unneeded console log

--- commands/newQuote.js
+++ commands/newQuote.js
@@ -8,7 +8,6 @@ async function addQuote(author, quoteString) {
       "name": `${author}`,
       "quote": `${quoteString}`
     })
-    console.log(json);
     fs.writeFile('quotes.json', JSON.stringify(json), (err) => { if (err) console.log(err) });
 })
 
